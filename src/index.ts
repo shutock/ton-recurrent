baa5315e6f3ecef843459b90bdd0851a8c517c17
Denis <yoshutock@gmail.com>
2024-08-09 18:49:01 +0500

    rafeuhbiuaef

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,49 @@
 import TonWeb from "tonweb";
 
-import dotenv from "dotenv";
-
-import { initWallet } from "./lib";
+import { mnemonicToPrivateKey } from "@ton/crypto";
+import { TonClient } from "@ton/ton";
 
 const { JettonWallet } = TonWeb.token.jetton;
 
 const jettonWalletAddress = "kQBnLQClf4K3xBml_4v8c2Yd9-5wEcLVfnt8P4vjO145h04P";
+const message = "Lorem";
 
-dotenv.config();
-const mnemonic = process.env.MNEMONIC?.split(" ");
-if (!mnemonic) throw new Error("MNEMONIC env variable is required");
+const data = {
+  "0:fc4e88908d24f8e342b56276ef2bf4a4142a5967987569f7a98c12080b195138": 50, // test
+  "0QBt8J4HItTLQ4fU5uPkmzHkUoBThsJVEfInMzn21Ukxkpmy": 100, // test
+};
+
+const mnemonic = [""];
+
+const endpoints = {
+  testnet: "https://testnet.toncenter.com/api/v2/jsonRPC",
+  mainnet: "https://toncenter.com/api/v2/jsonRPC",
+};
+
+const network: keyof typeof endpoints = "mainnet";
+
+const tonClient = new TonClient({
+  endpoint: endpoints[network],
+});
+
+const tonweb = new TonWeb(new TonWeb.HttpProvider(endpoints[network]));
 
 const transferJetton = async (to: string, amount: number) => {
-  const { secretKey } = await initWallet();
-
-  const tonweb = new TonWeb(
-    new TonWeb.HttpProvider("https://testnet.toncenter.com/api/v2/jsonRPC")
-  );
+  const { secretKey, publicKey } = await mnemonicToPrivateKey(mnemonic);
 
   const keyPair = TonWeb.utils.nacl.sign.keyPair.fromSecretKey(secretKey);
 
   const WalletClass = tonweb.wallet.all["v4R2"];
 
-  const wallet = new WalletClass(tonweb.provider, {
-    publicKey: keyPair.publicKey,
-  });
+  const wallet = new WalletClass(tonweb.provider, { publicKey });
+
+  const responseAddress = await wallet.getAddress();
 
   const seqno = (await wallet.methods.seqno().call()) || 0;
 
-  const comment = new Uint8Array([
+  const forwardPayload = new Uint8Array([
     ...new Uint8Array(4),
-    ...new TextEncoder().encode("gift"),
+    ...new TextEncoder().encode(message),
   ]);
 
   const jettonWallet = new JettonWallet(tonweb.provider, {
@@ -41,17 +53,18 @@ const transferJetton = async (to: string, amount: number) => {
   console.log(
     await wallet.methods
       .transfer({
-        sendMode: 1,
+        sendMode: 3,
         secretKey: keyPair.secretKey,
         toAddress: jettonWalletAddress,
         amount: TonWeb.utils.toNano("0.1"),
         seqno: seqno,
         payload: await jettonWallet.createTransferBody({
           toAddress: new TonWeb.utils.Address(to),
-          tokenAmount: TonWeb.utils.toNano("500"),
-          forwardAmount: TonWeb.utils.toNano("0.0005"),
-          forwardPayload: comment,
-          responseAddress: new TonWeb.utils.Address(to),
+          // @ts-ignore
+          jettonAmount: TonWeb.utils.toNano(amount.toString()),
+          forwardAmount: TonWeb.utils.toNano("0.000000001"),
+          forwardPayload,
+          responseAddress,
         }),
       })
       .send()
@@ -59,7 +72,17 @@ const transferJetton = async (to: string, amount: number) => {
 };
 
 (async () => {
-  console.log("Sending jetton...");
-  await transferJetton("0QBt8J4HItTLQ4fU5uPkmzHkUoBThsJVEfInMzn21Ukxkpmy", 10);
-  console.log("Jetton sent!");
+  console.log(`Starting...\n\n`);
+
+  const array = Object.entries(data);
+  let i = 0;
+
+  for (const [to, amount] of array) {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await transferJetton(to, amount);
+    console.log(`Sent ${i + 1}/${array.length}`);
+    i++;
+  }
+
+  console.log("\n\nDone!");
 })();
--- src/lib/ton-client.ts
+++ src/lib/ton-client.ts
@@ -7,7 +7,7 @@ const { IS_TESTNET, API_KEY } = process.env;
 
 const endpoints = {
   testnet: "https://testnet.toncenter.com/api/v2/jsonRPC",
-  mainnet: " https://toncenter.com/api/v2/jsonRPC",
+  mainnet: "https://toncenter.com/api/v2/jsonRPC",
 };
 
 export const tonClient = new TonClient({
